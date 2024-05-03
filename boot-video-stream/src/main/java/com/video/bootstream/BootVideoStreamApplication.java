350bf023a57310038ef3bc1861c5b52a79183a92
Learn Code With Durgesh Officials <55891143+LearnCodeWithDurgesh@users.noreply.github.com>
2024-05-03 22:25:19 +0530

    stream project added

---
+++ boot-video-stream/src/main/java/com/video/bootstream/BootVideoStreamApplication.java
@@ -0,0 +1,12 @@
+package com.video.bootstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootVideoStreamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootVideoStreamApplication.class, args);
+    }
+
+}
---
+++ boot-video-stream/src/test/java/com/video/bootstream/BootVideoStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.video.bootstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootVideoStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
